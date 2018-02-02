a38df85f60369f32d1bf4a982c3519abc84e84da
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2018-02-02 21:56:21 +0000

    Green test: postCounter is tracked in User; fixed Update test for post count

--- test/update_test.js
+++ test/update_test.js
@@ -52,10 +52,10 @@ describe('updating records', () => {
   })
 
   it('A user can have their postcount incremented by 1', (done) => {
-    User.update({ name: 'Joe' }, { $inc: { postCount: 1 } })
+    User.update({ name: 'Joe' }, { $inc: { likes: 1 } })
       .then(() => User.findOne({ name: 'Joe'}))
       .then((user) => {
-        assert(user.postCount === 1);
+        assert(user.likes === 1);
         done();
       })
   })
