6fb1ad5f6300a05cb9274cb19b76d4d46cfdefc4
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2018-02-02 13:31:56 +0000

    Green test: Model instance update

--- test/update_test.js
+++ test/update_test.js
@@ -10,15 +10,23 @@ describe('updating records', () => {
     .then(() => done());
   })
 
+  function assertName(operation,done){
+    operation
+    .then(() => User.find({}))
+    .then((users) => {
+      assert(users.length === 1);
+      assert(users[0].name === 'Alex');
+      done();
+    })
+  }
+
   it('instance type using set n save', (done) => {
     joe.set('name', 'Alex');
-    joe.save()
-      .then(() => User.find({}))
-      .then((users) => {
-        assert(users.length === 1);
-        assert(users[0].name === 'Alex');
-        done();
+    assertName(joe.save(), done);
+  })
 
-      })
+  it('A model instance can update', (done) => {
+
+    assertName(joe.update({name:'Alex'}), done)
   })
 })
