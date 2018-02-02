8103d52efefb7814f38a7fceceaeac1d25093d02
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2018-02-02 15:22:00 +0000

    Green test: added Post Count field, and can update

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('updating records', () => {
   let joe;
 
   beforeEach((done) => {
-    joe = new User({ name: 'Joe'});
+    joe = new User({ name: 'Joe', postCount: 0});
     joe.save()
     .then(() => done());
   })
@@ -45,9 +45,19 @@ describe('updating records', () => {
     );
   })
 
-  it('A model class can find a record with an ID and update', done => {
+  it('A model class can find a record with an ID and update', (done) => {
     assertName(
     User.findByIdAndUpdate(joe._id, { name: 'Alex' }),
     done)
   })
+
+  it('A user can have their postcount incremented by 1', (done) => {
+    User.update({ name: 'Joe' }, { $inc: { postCount: 1 } })
+      .then(() => User.findOne({ name: 'Joe'}))
+      .then((user) => {
+        assert(user.postCount === 1);
+        done();
+      })
+  })
+
 })
