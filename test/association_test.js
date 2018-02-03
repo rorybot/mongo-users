a66c7c5210740dc4610948e5a59df15aab77e2aa
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2018-02-03 12:24:09 +0000

    Green: can save a full relation tree

--- test/association_test.js
+++ test/association_test.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const asser = require('assert');
+const assert = require('assert');
 const User = require('../src/user');
 const Comment = require('../src/comment');
 const BlogPost = require('../src/blogPost');
@@ -30,9 +30,32 @@ describe('Assocations', () => {
     User.findOne({ name: 'Joe'})
       .populate('blogPosts')
       .then((user) => {
-        console.log(user);
+        assert(user.blogPosts[0].title === 'Rory is great')
         done();
       })
   });
+
+  it('saves a full relation tree', (done) => {
+    User.findOne({ name: 'Joe' })
+    .populate({
+      path: 'blogPosts',
+      populate: {
+        path: 'comments',
+        model: 'comment',
+        populate: {
+          path: 'user',
+          model: 'user'
+        }
+      }
+    })
+    .then((user) => {
+      assert(user.name === 'Joe');
+      assert(user.blogPosts[0].title === 'Rory is great');
+      assert(user.blogPosts[0].comments[0].content === 'I disagree! He is the worst!');
+      assert(user.blogPosts[0].comments[0].user.name === 'Joe')
+
+      done()
+    })
+  })
 
 })
